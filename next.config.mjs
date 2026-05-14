d931086d5b35513c48489f15e9792af0c09b72fa
alexvu05 <huuvd05@gmail.com>
2026-05-14 17:02:34 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,12 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     },
+    eslint: {
+      ignoreDuringBuilds: true,  
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
     async headers() {
       return [
         {
